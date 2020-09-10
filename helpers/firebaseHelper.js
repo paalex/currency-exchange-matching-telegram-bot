a079cee7f4289899386c82e5859328bde069fb38
paalex <salexp@gmail.com>
2020-09-10 06:10:53 -0400

    debugging

--- helpers/firebaseHelper.js
+++ helpers/firebaseHelper.js
@@ -35,6 +35,7 @@ var offersRef = db.child("offers");
 
 export async function storeUser(user) {
   if (!user) throw new Error('no user to save');
+  console.log('storeUser',storeUser)
   const userRef = usersRef.child(user.id);
   return userRef.update(user);
 }
